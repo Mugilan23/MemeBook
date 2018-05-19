4302ee17ce85cff911cdfa1ecd59da80f8c48370
Mugilan E.S <mugilanmass23@gmail.com>
2018-05-19 12:52:01 +0530

    Created Splash Screen to Move to Main Activity within 3 Seconds, String, Color, and Style also added.

---
+++ app/src/main/java/com/mamudev/memebook/SplashPage.java
@@ -0,0 +1,31 @@
+package com.mamudev.memebook;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashPage extends AppCompatActivity {
+
+    private static int TIME_OUT = 3000; //Time to Launch Another Activity.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setTheme(R.style.SplashTheme);
+        setContentView(R.layout.activity_splash_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashPage.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+
+            }
+        }, TIME_OUT);
+
+    }
+}
