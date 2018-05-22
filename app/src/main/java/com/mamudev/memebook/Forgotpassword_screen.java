b4ff20107abb779a03e106ec7fb6e7a9eca831ad
mani3 <mani37105@gmail.com>
2018-05-21 21:44:13 -0700

    changes i add another screen for forgotpassword. click forgotpassword textview. After this call me and info to me

--- app/src/main/java/com/mamudev/memebook/Forgotpassword_screen.java
+++ app/src/main/java/com/mamudev/memebook/Forgotpassword_screen.java
@@ -5,22 +5,18 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class Login_screen extends AppCompatActivity {
-    TextView forgotPassword;
+public class Forgotpassword_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_forgotpassword_screen);
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
-        forgotPassword=findViewById(R.id.tvForgotpassword);
-
     }
 
-    public void gotoForgotPassword(View view) {
-        startActivity(new Intent(this,Forgotpassword_screen.class));
+    public void backToLogin(View view) {
+        startActivity(new Intent(this,Login_screen.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
     }
 }
