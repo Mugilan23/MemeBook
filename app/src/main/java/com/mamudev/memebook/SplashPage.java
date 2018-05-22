4504821f97646737d92cf193dd4421ad0bcce008
mani3 <mani37105@gmail.com>
2018-05-21 19:42:45 -0700

    changes Add some background and animation affect icon in splash screen. please run and check

--- app/src/main/java/com/mamudev/memebook/SplashPage.java
+++ app/src/main/java/com/mamudev/memebook/SplashPage.java
@@ -10,6 +10,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 public class SplashPage extends AppCompatActivity {
+    LinearLayout l1;
+    Animation animation;
 
 
     private static int TIME_OUT = 3000; //Time to Launch Another Activity.
@@ -21,6 +23,10 @@ public class SplashPage extends AppCompatActivity {
         setContentView(R.layout.activity_splash_page);
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
+        l1=findViewById(R.id.LinearLayout);
+        animation=AnimationUtils.loadAnimation(this,R.anim.uptodown_logo_anim);
+        l1.setAnimation(animation);
+
 
 
         //Splash Screen to Next Activity
