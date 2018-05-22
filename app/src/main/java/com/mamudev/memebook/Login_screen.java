9305e94709f4184a7067a9a4ef39624e442a909c
mani3 <mani37105@gmail.com>
2018-05-21 20:53:02 -0700

    changes i add login screen and i did not write java code because after u  finish singup i processed. mukilan u add signup screen

--- app/src/main/java/com/mamudev/memebook/Login_screen.java
+++ app/src/main/java/com/mamudev/memebook/Login_screen.java
@@ -1,5 +1,6 @@
 package com.mamudev.memebook;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class Login_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
     }
 }
