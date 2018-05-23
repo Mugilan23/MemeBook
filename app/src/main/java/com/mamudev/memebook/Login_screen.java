cf995fa5ec8d8339989fe2402b53fb0eff4c3199
mani3 <mani37105@gmail.com>
2018-05-23 04:47:04 -0700

    changes i add some navigation activity

--- app/src/main/java/com/mamudev/memebook/Login_screen.java
+++ app/src/main/java/com/mamudev/memebook/Login_screen.java
@@ -5,10 +5,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login_screen extends AppCompatActivity {
     TextView forgotPassword;
+    Button btlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,14 @@ public class Login_screen extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
         forgotPassword=findViewById(R.id.tvForgotpassword);
+        btlogin=findViewById(R.id.btLogin);
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login_screen.this,MainActivity.class));
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/mamudev/memebook/MainActivity.java
+++ app/src/main/java/com/mamudev/memebook/MainActivity.java
@@ -1,13 +1,67 @@
 package com.mamudev.memebook;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.navigation);
+        View navView=navigationView.inflateHeaderView(R.layout.navigation_header);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
     }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_profile:
+                Toast.makeText(this,"profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_post:
+                Toast.makeText(this,"post",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this,"home",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_friends:
+                Toast.makeText(this,"friends",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_find_friends:
+                Toast.makeText(this,"find friends",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this,"logout",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this,"settings",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_message:
+                Toast.makeText(this,"message",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
 }
